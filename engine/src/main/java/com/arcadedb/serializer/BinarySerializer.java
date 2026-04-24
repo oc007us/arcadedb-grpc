c96d3abe98c19d4efe3a67697c4ac3d41e56d51c
Luca Garulli <lvca@users.noreply.github.com>
2026-04-24 15:45:17 -0400

    fix: binary serializer now throws an exception in case of corruption

--- engine/src/main/java/com/arcadedb/serializer/BinarySerializer.java
+++ engine/src/main/java/com/arcadedb/serializer/BinarySerializer.java
@@ -811,8 +811,7 @@ public class BinarySerializer {
     }
 
     default:
-      LogManager.instance().log(this, Level.INFO, "Error on deserializing value of type " + type);
-      value = null;
+      throw new SerializationException("Error on deserializing value of unknown type " + type);
     }
     return value;
   }
